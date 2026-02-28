76f9c6f9d2d6ac7003cb3cb34eef2f2aa2a15721
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2026-02-27 19:49:07 -0800

    basic POST endpoint setup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import HomePage from './endpoints/HomePage.js'
+import HomePage from './endpoints/homepage.js';
+import { randomUUID } from 'crypto';
 
 const app = express()
 const homepage = new HomePage()
@@ -8,15 +9,33 @@ app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/', (request, response) => {
-    get_data = homepage.get()
-    console.log(get_data)
-    response.send(get_data)
+    response.send(homepage.get())
 })
+
+function createBookDataInstance(data) {
+    return {
+        id: randomUUID(),
+        book_name: data.book_name ?? null,
+        chapters_read: data.chapters_read ?? null,
+        favorite_chapters: data.favorite_chapters ?? null,
+        thoughts_about_book: data.thoughts_about_book ?? null,
+        created_at: Date.now() ?? null,
+    }
+}
 
 app.post('/', (request, response) => {
     const data = request.body
-    console.log(`POST request data=${data}`)
-    response.send('Data found.')
+    createBookDataInstance(request.body)
+    console.log(`POST request data=${data.chapters_read}`)
+    response.send(homepage.post())
+})
+
+app.put('/', (request, response) => {
+    response.send(homepage.update())
+})
+
+app.delete('/', (request, response) => {
+    response.send(homepage.delete())
 })
 
 app.listen(3000, () => {
